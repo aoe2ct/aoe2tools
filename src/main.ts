5156f6315be13a15103813ad111ff433b3d051a8
Stéphane Bisinger <stephane@sbisinger.ch>
2025-12-08 23:41:24 +0100

    Prefer using css media matcher when no override set

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,10 @@ app.mount('#app')
 
 // Apply saved theme on page load
 document.addEventListener('DOMContentLoaded', () => {
-  const fallbackTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
-  const savedTheme = localStorage.getItem('theme') || fallbackTheme;
+  if (!('theme' in localStorage)) {
+    return
+  }
+  const savedTheme = localStorage.getItem('theme') || 'light';
 
   document.documentElement.setAttribute('data-theme', savedTheme);
 });
