816ec460b94b09b2992a42de18c95ce5d8213d3d
Stéphane Bisinger <stephane@sbisinger.ch>
2026-01-19 01:07:21 +0100

    Update imports for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,27 +5,30 @@
  */
 
 // Composables
-import { useAuthStore } from '@/store/auth'
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto-routes'
+import { useAuthStore } from "@/store/auth";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "vue-router/auto-routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-router.beforeResolve(async to => {
-  if (to.path != '/auth') {
-    return
+router.beforeResolve(async (to) => {
+  if (to.path != "/auth") {
+    return;
   }
   if (!to.query?.code) {
-    return { path: '/' }
+    return { path: "/" };
   }
   const authStore = useAuthStore();
-  const authResponse = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth?code=${to.query.code}&state=${to.query.state}`, { credentials: 'include' })
-  const authTokens = await authResponse.text()
+  const authResponse = await fetch(
+    `${import.meta.env.VITE_API_BASE_URL}/auth?code=${to.query.code}&state=${to.query.state}`,
+    { credentials: "include" },
+  );
+  const authTokens = await authResponse.text();
   authStore.setCredentials(authTokens);
-  return { path: '/' }
-})
+  return { path: "/" };
+});
 
-export default router
+export default router;
